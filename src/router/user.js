67af4af76d3adcf9e68c13d64729f0a04e4f2294
chen_lei <2624304501@qq.com>
2019-07-17 19:41:56 +0800

    remvoe unused

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,6 @@
 let express = require('express')
 let router = express.Router()
-const bodyParser = require('body-parser')
+
 
 router.post("/check", (req, res) => {
     const { user, password } = req.body
