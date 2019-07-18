e51331f3d3bfc83f40f59a55c0a691f9e1ef3f3e
chen_lei <2624304501@qq.com>
2019-07-18 11:35:09 +0800

    cc

--- src/router/user.js
+++ src/router/user.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let router = express.Router()
 
-
+//TODO 增加 json web token
 router.post("/check", (req, res) => {
     const { user, password } = req.body
     res.json({user,password})
